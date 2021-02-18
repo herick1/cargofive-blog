38e90994adc9ac6f3b93bb297c84b1358fdcc4e6
herick1 <herick200@gmail.com>
2021-02-18 22:35:00 +0100

    Fix: Arreglando errores con el envio de correos para el recuperar password

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+                <div class="card-header"><h3>{{ __('Login') }}</h3></div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('login') }}">
@@ -58,7 +58,7 @@
                                 </button>
 
                                 @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
+                                    <a class="btn btn-link" href="/forget-password">
                                         {{ __('Forgot Your Password?') }}
                                     </a>
                                 @endif
