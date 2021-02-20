1ba3ccdb57ac38c4268aee613812062510c5723b
herick1 <herick200@gmail.com>
2021-02-20 07:12:04 -0800

    Fix: Colocando header del /auth/  en los sitios corectos

--- resources/views/auth/passwords/confirm.blade.php
+++ resources/views/auth/passwords/confirm.blade.php
@@ -1,11 +1,13 @@
 @extends('layouts.app')
-
+@section('title')
+{{ __('Confirm Password') }}
+@endsection
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Confirm Password') }}</div>
+                <div class="card-header"></div>
 
                 <div class="card-body">
                     {{ __('Please confirm your password before continuing.') }}
--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -1,11 +1,13 @@
 @extends('layouts.app')
-
+@section('title')
+{{ __('Reset Password') }}
+@endsection
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header"><h3>{{ __('Reset Password') }}</h3></div>
+                <div class="card-header"></div>
 
                 <div class="card-body">
                     <form method="POST" action="/reset-password">
